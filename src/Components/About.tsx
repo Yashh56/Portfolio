b270330089e16b677c61616639aa5c8027aa85be
Yash <yashsaini18166@gmail.com>
2024-05-09 12:46:10 +0530

    Resume Updated Minor Changes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -21,25 +21,26 @@ const About = () => {
 
     return (
         <div id='animatedSection' className='flex  bg-[#171717] flex-col justify-center max-md:p-4 items-center'>
-            <h1 id='animatedSection' className='text-center text-3xl m-4'>About Me</h1>
+            <h1 id='animatedSection' className='text-center text-3xl m-4'>ABOUT ME</h1>
             <div className={`flex items-center justify-center ${isVisible ? 'animate-fade-right animate-duration-[1000ms] animate-ease-out' : ' '}`}>
                 <div className='flex-col flex justify-center items-center text-left'>
                     <Image
                         className='flex items-center justify-center md:w-96 md:h-96 w-72 h-72'
                         borderRadius='full'
-                        boxSize='150px'
-                        src='1.jpg'
-                        alt='Segun Adebayo'
+                        boxSize='180px'
+                        src='https://i.pinimg.com/564x/37/3f/f4/373ff491701d97ecfded705bc7d9955e.jpg'
                     />
-                    <div className='flex-col flex-wrap md:w-fit justify-center items-center md:text-2xl'>
-                        <p>🙋‍♂️ Hello, I'm Yash, a first-year undergraduate student pursuing a Bachelor's in Computer Science.</p>
-                        <p> ⚒️I'm passionate about exploring various facets of the field, including <mark>web development</mark> ,</p>
-                        <p><mark>competitive programming</mark>, and other related areas.</p>
-                        <p>🚀 Currently I am learning different DSA Concepts and working on projects related to MERN Stack.</p>
+                    <div className='flex-col grid grid-row-3 gap-2 md:w-[80%] md:text-2xl'>
+                        <p>🙋‍♂️ I am first-year undergraduate student pursuing a Bachelor's in Computer Science.</p>
+                        <p> ⚒️I'm passionate about exploring various facets of the field, including <mark>web development</mark> ,<mark>competitive programming</mark>, and other related areas</p>
+                        {/* <p>.</p> */}
+                        <p>🚀 Currently I am learning different Data Structure and Algorithms Concepts and working on projects related to MERN Stack.</p>
                         <p>✨ I also Contributed in <mark> Hacktoberfest 2023</mark>.</p>
+                        <p>🐋 I love problem solving and doing competitive programming.</p>
+                        <p> 🍁 My hobbies are watching anime,cricket, andreading books .</p>
                     </div>
                     <div className='flex max-md:mt-12 md:mt-12 float-left'>
-                        <Link to={'https://drive.google.com/file/d/106sD2ePacEmWaFaxcPxT1LN-czwDApB9/view?usp=sharing'} target='_blank' >
+                        <Link to={'https://drive.google.com/file/d/1qtIjPWox_-LRbyTZ82PzqG6UlimrxeAI/view?usp=drive_link'} target='_blank' >
                             <Button size={'lg'} className='Button'>Resume</Button>
                         </Link>
                     </div>
--- src/Pages/AboutMe.tsx
+++ src/Pages/AboutMe.tsx
@@ -2,65 +2,64 @@ import { Image, Text } from '@chakra-ui/react'
 import { SiPrisma } from 'react-icons/si'
 import { Link } from 'react-router-dom'
 
-export default function AboutMe() {
+const AboutMe = () => {
   return (
-    <div className='flex justify-center bg-[#171717] items-center h-screen w-full'>
-      <div>
-        <h1 className='text-center m-4 text-4xl'>ABOUT ME</h1>
-        <div className='text-xl'>
-          <p>
+    <div className='max-md:h-[120vh] md:h-[100vh] w-full flex items-center justify-center'>
+      <div className=''>
+        <h1 className='text-center text-4xl'>ABOUT ME</h1>
+
+        <div className='flex-col mt-4 md:text-xl'>
+          <p className='mt-3'>
             ▫️ Hii Everyone, My name is Yash Saini
           </p>
-          <p>
+          <p className='mt-3'>
             ▫️  I'm a first-year undergraduate student pursuing a Bachelor's in Computer Science.
           </p>
-          <p>
+          <p className='mt-3'>
             ▫️ I am a <Text as='mark'>Full Stack Developer</Text> and currently I am exploring new blockchain technologies and data structures and algorithms.
           </p>
-          <p>
+          <p className='mt-3'>
             ▫️ I am also a <Text as='mark'>competitive programmer</Text> and I have participated in various competitions.
           </p>
-          <p>
+          <p className='mt-3'>
+            ▫️ I also contribute to open source project called  <Link className='hover:cursor-pointer hover:underline' to={'https://github.com/devicons/devicon'}>Devicons</Link>.
+          </p>
+          <p className='mt-3'>
             ▫️ I am also looking for internships and opportunities to work with amazing people.
           </p>
-          <p>
-            ▫️ My Rating on Codeforces: 1037
-          </p>
-          <p>
+          <p className='mt-3'>
             ▫️ My Highest Rating on LeetCode: 1746
           </p>
         </div>
-        <h1 className='md:mt-2 md:text-3xl max-md:text-2xl md:m-4'>
-          Tech Stack I am familiar with:
-        </h1>
-        <div className='flex flex-wrap max-md:text-sm gap-2 ml-4'>
-          <i className="devicon-javascript-plain colored"></i>
-          <i className="devicon-typescript-plain colored"></i>
-          <i className="devicon-react-original-wordmark colored"></i>
-          <i className="devicon-nodejs-plain colored"></i>
-          <i className="devicon-express-original"></i>
-          <i className="devicon-nextjs-plain"></i>
-          <i className="devicon-solidity-plain"></i>
-          <i className="devicon-mongodb-plain-wordmark colored"></i>
-          <i className="devicon-mysql-plain-wordmark colored"></i>
-          <i className='text-white'><SiPrisma /></i>
-          <i className="devicon-python-plain colored"></i>
-          <i className="devicon-git-plain colored"></i>
-          <i className="devicon-docker-plain colored"></i>
+        <div>
+          <h1 className='text-3xl mt-8'> Tech Stack I am familiar with:</h1>
+          <div className='flex max-md:justify-evenly flex-wrap gap-4'>
+            <i className="devicon-javascript-plain colored"></i>
+            <i className="devicon-typescript-plain colored"></i>
+            <i className="devicon-react-original-wordmark colored"></i>
+            <i className="devicon-nodejs-plain colored"></i>
+            <i className="devicon-express-original"></i>
+            <i className="devicon-nextjs-plain"></i>
+            <i className="devicon-solidity-plain"></i>
+            <i className="devicon-mongodb-plain-wordmark colored"></i>
+            <i className="devicon-mysql-plain-wordmark colored"></i>
+            <i className='text-white'><SiPrisma /></i>
+            <i className="devicon-python-plain colored"></i>
+            <i className="devicon-git-plain colored"></i>
+            <i className="devicon-docker-plain colored"></i>
+          </div>
         </div>
-
-        <h1 className='md:text-3xl max-md:text-2xl md:mt-2 md:m-4'>
-          Platforms I use for Competitive Programming:
-        </h1>
-        <div className='flex'>
-          <Link to='https://leetcode.com/yashsaini18166/' target='_blank'>
+        <div className='mt-6'>
+          <h1 className='text-2xl'>Platforms I use for Competitive Programming</h1>
+        <div>
+        <Link to='https://leetcode.com/yashsaini18166/' target='_blank'>
             <Image src="LeetCode.svg" alt="LeetCode" width='20' height='10' />
           </Link>
-          <Link to='https://codeforces.com/profile/yashh18' target='_blank'>
-            <Image src="codeforces.svg" alt="Codeforces" width='20' height='10' />
-          </Link>
+        </div>
         </div>
       </div>
     </div>
   )
 }
+
+export default AboutMe
